28a7b63bdebb4fdbaf1dcbf7be67bb950fe5f992
Nikolas Santis Escalante <nikosantis@MacBook-Air-de-Nikolas.local>
2019-10-22 18:24:12 -0300

    Commit Final Curso

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, compose } from 'redux';
 import { Router } from 'react-router';
 import { createBrowserHistory } from 'history';
 import reducer from './reducers';
-import initialState from './initialState';
 import App from './routes/App';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+if (typeof window !== 'undefined') {
+  let composeEnhancers;
+  if (process.env.NODE_ENV === 'production') composeEnhancers = compose;
+  else composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const preloadedState = window.__PRELOADED_STATE__;
+  const store = createStore(reducer, preloadedState, composeEnhancers());
+  const history = createBrowserHistory();
 
-const store = createStore(reducer, initialState, composeEnhancers());
-const history = createBrowserHistory();
+  hydrate(
+    <Provider store={store}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </Provider>,
+    document.getElementById('app'),
+  );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('app'),
-);
+};
--- src/server/render/index.js
+++ src/server/render/index.js
@@ -1,18 +1,30 @@
-const render = (html) => {
-  return (`
+import getManifest from '../getManifest';
+
+const files = getManifest();
+
+const render = (html, preloadedState) => {
+  return `
     <!DOCTYPE html>
     <html>
       <head>
         <title>Platzi Video</title>
-        <link rel="stylesheet" href="assets/app.css" type="text/css"/>
+        <link rel="stylesheet" href="${files['main.css']}" type="text/css"/>
       </head>
       <body>
         <div id="app">${html}</div>
-        <script src="assets/app.js" type="text/javascript"></script>
-        <script src="assets/vendor.js" type="text/javascript"></script>
+        <script>
+          // WARNING: See the following for security issues around embedding JSON in HTML:
+          // http://redux.js.org/recipes/ServerRendering.html#security-considerations
+          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(
+            /</g,
+            "\\u003c"
+          )}
+        </script>
+        <script src="${files['main.js']}" type="text/javascript"></script>
+        <script src="${files['vendors.js']}" type="text/javascript"></script>
       </body>
     </html>
-  `);
+  `;
 };
 
 export default render;
